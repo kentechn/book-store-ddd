f719c78260cd595161bda2e2a033145bfa625b71
hiroka <mitsunagahrk87@gmail.com>
2025-09-28 14:39:58 +0900

    feat:ユースケースの実装

---
+++ backend/src/query/types/book.ts
@@ -0,0 +1,11 @@
+export type Book = {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+  isPaperbook: boolean;
+  publishAt: Date;
+  isSaled: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
